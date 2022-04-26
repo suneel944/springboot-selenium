6c785b813eeb68ef78e48a0065a74578a79383df
suneel944 <suneel944@gmail.com>
2022-04-26 11:59:01 +0530

    added project dependencies

---
+++ src/main/java/com/auto/spring/springselenium/SpringSeleniumApplication.java
@@ -0,0 +1,13 @@
+package com.auto.spring.springselenium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSeleniumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSeleniumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/auto/spring/springselenium/SpringSeleniumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auto.spring.springselenium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSeleniumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

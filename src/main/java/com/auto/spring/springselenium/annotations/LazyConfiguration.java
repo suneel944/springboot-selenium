1af50f34012d91e12ef4f8270a120d600d6084a3
suneel944 <suneel944@gmail.com>
2022-04-29 05:08:20 +0530

    added custom annotations

---
+++ src/main/java/com/auto/spring/springselenium/annotations/LazyConfiguration.java
@@ -0,0 +1,15 @@
+package com.auto.spring.springselenium.annotations;
+
+import java.lang.annotation.*;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Configuration;
+
+
+@Lazy
+@Configuration
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+public @interface LazyConfiguration
+{
+}
---
+++ src/main/java/com/auto/spring/springselenium/annotations/Page.java
@@ -0,0 +1,16 @@
+package com.auto.spring.springselenium.annotations;
+
+import java.lang.annotation.*;
+import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
+
+@Lazy
+@Component
+@Scope("prototype")
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+public @interface Page
+{
+}

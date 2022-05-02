74ccd3df1a7f3b1ca13a4174b31a72fb859ba977
suneel944 <suneel944@gmail.com>
2022-05-02 16:57:25 +0530

    code refactoring

---
+++ src/main/java/com/auto/spring/springselenium/framework/service/Launcher.java
@@ -0,0 +1,25 @@
+package com.auto.spring.springselenium.framework.service;
+
+import org.openqa.selenium.WebDriver;
+import org.springframework.stereotype.Service;
+import com.auto.spring.springselenium.framework.annotations.LazyAutowired;
+
+
+@Service
+public class Launcher
+{
+    @LazyAutowired
+    private WebDriver driver;
+
+    public boolean launchUrl(String url)
+    {
+        try
+        {
+            this.driver.get(url);
+            return true;
+        } catch (Exception e)
+        {
+            return false;
+        }
+    }
+}

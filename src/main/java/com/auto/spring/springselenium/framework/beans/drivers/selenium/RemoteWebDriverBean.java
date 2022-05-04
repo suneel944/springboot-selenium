c761461815410909e531e25631501aa51120a4f4
suneel944 <suneel944@gmail.com>
2022-05-04 15:04:53 +0530

    added supportive code for injecting options for different browsers

--- src/main/java/com/auto/spring/springselenium/framework/beans/drivers/selenium/RemoteWebDriverBean.java
+++ src/main/java/com/auto/spring/springselenium/framework/beans/drivers/selenium/RemoteWebDriverBean.java
@@ -2,12 +2,12 @@ package com.auto.spring.springselenium.framework.beans.drivers.selenium;
 
 import java.net.URL;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxOptions;
 import org.springframework.context.annotation.Profile;
 import org.springframework.beans.factory.annotation.Value;
+import com.auto.spring.springselenium.framework.service.BrowserOptions;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import com.auto.spring.springselenium.framework.annotations.others.LazyAutowired;
 import com.auto.spring.springselenium.framework.annotations.others.LazyConfiguration;
 import com.auto.spring.springselenium.framework.annotations.parallelexecution.ThreadScopeBean;
 
@@ -15,18 +15,21 @@ import com.auto.spring.springselenium.framework.annotations.parallelexecution.Th
 @LazyConfiguration
 public class RemoteWebDriverBean
 {
+    @LazyAutowired
+    private BrowserOptions browserOptions;
+
     @Value("${selenium.grid.url}")
     private URL url;
 
     @ThreadScopeBean
     @ConditionalOnProperty(name = "browser", havingValue = "firefox")
     public WebDriver remoteFirefoxDriver(){
-        return new org.openqa.selenium.remote.RemoteWebDriver(this.url, new FirefoxOptions());
+        return new org.openqa.selenium.remote.RemoteWebDriver(this.url, browserOptions.getFirefoxOptions());
     }
 
     @ThreadScopeBean
     @ConditionalOnMissingBean
     public WebDriver remoteChromeDriver(){
-        return new org.openqa.selenium.remote.RemoteWebDriver(this.url, new ChromeOptions());
+        return new org.openqa.selenium.remote.RemoteWebDriver(this.url, browserOptions.getChromeOptions());
     }
 }

4a15cc78b66d21cfa60303356bd97e1391b66025
suneel944 <suneel944@gmail.com>
2022-05-05 19:21:20 +0530

    added logging to remote web driver

--- src/main/java/com/auto/spring/springselenium/framework/beans/drivers/selenium/RemoteWebDriverBean.java
+++ src/main/java/com/auto/spring/springselenium/framework/beans/drivers/selenium/RemoteWebDriverBean.java
@@ -1,6 +1,7 @@
 package com.auto.spring.springselenium.framework.beans.drivers.selenium;
 
 import java.net.URL;
+import lombok.extern.log4j.Log4j2;
 import org.openqa.selenium.WebDriver;
 import org.springframework.context.annotation.Profile;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,6 +14,7 @@ import com.auto.spring.springselenium.framework.annotations.parallelexecution.Th
 
 @Profile("remote")
 @LazyConfiguration
+@Log4j2
 public class RemoteWebDriverBean
 {
     @LazyAutowired
@@ -24,12 +26,14 @@ public class RemoteWebDriverBean
     @ThreadScopeBean
     @ConditionalOnProperty(name = "browser", havingValue = "firefox")
     public WebDriver remoteFirefoxDriver(){
+        log.info("launching remote firefox web driver : {}", this.url);
         return new org.openqa.selenium.remote.RemoteWebDriver(this.url, browserOptions.getFirefoxOptions());
     }
 
     @ThreadScopeBean
     @ConditionalOnMissingBean
     public WebDriver remoteChromeDriver(){
+        log.info("launching remote chrome web driver : {}", this.url);
         return new org.openqa.selenium.remote.RemoteWebDriver(this.url, browserOptions.getChromeOptions());
     }
 }
